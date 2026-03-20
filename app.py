2fb2b127caa8c7e277817f4b612b0b3849f20b0c
chaostrader913 <liuhongbing913@gmail.com>
2026-03-20 09:59:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ page_intro = st.Page("pages/01_intro.py", title="Introduction", icon="🏠")
 page_chart = st.Page("pages/02_Chart_Grid.py", title="Chart Grid", icon="📊")
 page_scanner = st.Page("pages/03_Signal_Scanner.py", title="Signal Scanner", icon="📡")
 page_mtf = st.Page("pages/04_MTF_Analyzer.py", title="MTF Analyzer", icon="🚀")
-
+page_bump = st.Page("pages/05_bump_chart.py", title="Sector Rotation", icon="📈")
 
 # 2. Initialize the navigation
 # Passing them in a list dictates the order they appear in the sidebar
-pg = st.navigation([page_intro, page_chart, page_scanner,page_mtf])
+pg = st.navigation([page_intro, page_chart, page_scanner,page_mtf,page_bump])
 
 # 3. Run the selected page
 pg.run()
